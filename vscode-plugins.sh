79e7018f616bf0ed6e171ff36067ef5a50dad9e0
gabriel-roque <gabrielrms.dll@gmail.com>
2020-07-27 17:51:46 -0300

    feat: 🚀 update plugins and settings

--- vscode-plugins.sh
+++ vscode-plugins.sh
@@ -13,13 +13,13 @@ code --install-extension formulahendry.auto-rename-tag
 code --install-extension formulahendry.code-runner
 code --install-extension isudox.vscode-jetbrains-keybindings
 code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension mhutchie.git-graph
 code --install-extension mikestead.dotenv
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension naumovs.color-highlight
 code --install-extension PKief.material-icon-theme
 code --install-extension ritwickdey.LiveServer
-code --install-extension rokoroku.vscode-theme-darcula
 code --install-extension royaction.color-manager
 code --install-extension xabikos.JavaScriptSnippets
 code --install-extension Zignd.html-css-class-completion
